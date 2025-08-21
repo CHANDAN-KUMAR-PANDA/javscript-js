ed34310504a5c9e88c5cfb0aa91c6c041f776335
CHANDAN-KUMAR-PANDA <chandankumarpanda9007@gmail.com>
2025-08-21 11:22:14 +0530

    biggest number check

---
+++ If-else/prog4.js
@@ -0,0 +1,11 @@
+// biggest one
+var num1 = 55;
+var num2 = 68;
+var num3 = 72;
+if (num1 > num2 && num1 > num3) {
+  console.log("biggest one is:", num1);
+} else if (num2 > num1 && num2 > num3) {
+  console.log("biggest one is:", num2);
+} else {
+  console.log("biggest one is:", num3);
+}
